eaa004a9c8f71c1887c1eeabb6fc7884b00866b9
armin-etemad <a.ghetemad@gmail.com>
2025-08-01 00:50:57 +0200

    added testing for convergence of the intergral

--- tests/sph_basics.rs
+++ tests/sph_basics.rs
@@ -6,9 +6,10 @@ fn init_grid_n() {
     let spacing = 0.12; // spacing < h for overlap
     let rho_0 = 1000.0;
     let k = 3.0;
+    let mu = 0.1;
     let m = rho_0  * spacing * spacing;
 
-    let mut sph = SPHState::new(h, rho_0, k, m);
+    let mut sph = SPHState::new(h, rho_0, k, mu, m);
     sph.init_grid(10, 5, spacing);
     assert_eq!(sph.particles.len(), 50); // 10 * 5
     assert_eq!(sph.particles[0].pos, glam::Vec2::new(0.0, 0.0)); // 0 * 0.12, 0 * 0.12
@@ -22,9 +23,10 @@ fn grid_contains_all_particles() {
     let spacing = 0.08; // spacing < h for overlap
     let rho_0 = 1000.0;
     let k = 3.0;
+    let mu = 0.1;
     let m = rho_0  * spacing * spacing;
 
-    let mut sph = SPHState::new(h, rho_0, k, m);
+    let mut sph = SPHState::new(h, rho_0, k, mu, m);
     sph.init_grid(4, 3, spacing); // 4 * 3 = 12 particles
     let grid = sph.build_grid();
 
@@ -38,9 +40,10 @@ fn uniform_density_compare_to_rho_0() {
     let spacing = 0.04; // spacing < h for overlap
     let rho_0 = 1000.0;
     let k = 3.0;
+    let mu = 0.1;
     let m = rho_0 * spacing * spacing;
 
-    let mut sph = SPHState::new(h, rho_0, k, m);
+    let mut sph = SPHState::new(h, rho_0, k, mu, m);
     sph.init_grid(6, 6, spacing); 
     sph.density_pressure_calc();
     let max_rel_err = sph.particles
@@ -48,4 +51,20 @@ fn uniform_density_compare_to_rho_0() {
                         .map(|p| ((p.rho - sph.rho_0) / sph.rho_0).abs())
                         .fold(0.0, f32::max); // maximum relative error
     assert!(max_rel_err < 0.05, "relative error for density > 5%");
+}
+
+#[test]
+fn integral_no_nan() {
+    let h = 0.045;
+    let spacing = 0.04; // spacing < h for overlap
+    let rho_0 = 1000.0;
+    let k = 3.0;
+    let mu = 0.1;
+    let m = rho_0  * spacing * spacing;
+
+    let mut sph = SPHState::new(h, rho_0, k, mu, m);
+    sph.init_grid(10, 10, spacing);
+    for _ in 0..50 { sph.step(0.001); }
+    assert!(sph.particles.iter().all(|p| p.pos.is_finite()));
+
 }
