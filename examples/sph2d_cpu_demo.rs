56b2b82b5935654d232aa9f275a5c922b7228f19
armin-etemad <a.ghetemad@gmail.com>
2025-08-03 21:41:38 +0200

    adding toggle mode and cleaning up

--- examples/sph2d_cpu_demo.rs
+++ examples/sph2d_cpu_demo.rs
@@ -3,7 +3,18 @@ use bevy::sprite::Sprite;
 use bevy::input::ButtonInput;
 use bevy::window::PrimaryWindow;
 use glam::Vec2 as GVec2;
+
 use bevy_gpu_fluid::cpu::sph2d::SPHState;
+
+const RENDER_SCALE: f32 = 100.0;
+const PARTICLE_SIZE: f32 = 15.0;
+const DT: f32 = 0.0005;
+const X_MAX: f32 = 3.0;
+const X_MIN: f32 = -5.0;
+const BOUNCINESS: f32 = -3.0;
+const INTERACTION_AREA: f32 = 0.04; // when using mouse to interact
+const IMPULSE: f32 = 10.0; // when using mouse to interact
+const CYAN: Color = Color::srgb(0.0, 1.0, 1.0);
 
 #[derive(Component)]
 struct ParticleVisual(usize);
@@ -15,25 +26,43 @@ struct DragInput {
     pressed_down: bool, // left mouse button must be held
 }
 
-const RENDER_SCALE: f32 = 100.0;
-const PARTICLE_SIZE: f32 = 15.0;
-const DT: f32 = 0.0005;
+#[derive(Resource)]
+enum ViewMode {
+    ConstColor,
+    DensityColor,
+}
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(SPHState::demo_block_5k())
         .insert_resource(DragInput::default())
+        .insert_resource(ViewMode::DensityColor)
         .add_systems(Startup, setup)
         .add_systems(Update, (drag_input,
                               sph_step, 
                               apply_drag,
+                              toggle_view,
                               sync_particles,
                              )
                     )
         .run();
 }
 
+// toggle between the view modes
+fn toggle_view(
+    keys: Res<ButtonInput<KeyCode>>,
+    mut view: ResMut<ViewMode>,
+) {
+    if keys.just_pressed(KeyCode::Space) {
+        *view = match *view {
+            ViewMode::ConstColor => ViewMode::DensityColor,
+            ViewMode::DensityColor => ViewMode::ConstColor,
+        }
+    }
+}
+
+// from blue to red based on the density
 fn density_color(t: f32) -> Color {
     let t = t.clamp(0.0, 1.0);
     if t < 0.5 {
@@ -48,6 +77,7 @@ fn density_color(t: f32) -> Color {
     }
 }
 
+// by pressing left mouse button, fluid is "touched"
 fn drag_input(
     mut drag: ResMut<DragInput>,
     buttons: Res<ButtonInput<MouseButton>>,
@@ -90,37 +120,35 @@ fn apply_drag(
 
     let cursor_world = GVec2::new(
         (drag.screen_pos.x - win_w * 0.5) / RENDER_SCALE,
-        (- drag.screen_pos.y + win_h * 0.5) / RENDER_SCALE, // y downward is positive in bevy
+        (- drag.screen_pos.y + win_h * 0.5) / RENDER_SCALE,
     );
     let force_dir = GVec2::new(
         drag.delta.x / RENDER_SCALE,
-        -drag.delta.y /RENDER_SCALE, // y downward is positive in bevy
+        -drag.delta.y /RENDER_SCALE,
     );
-
-    let radius2 = 0.2*0.2;
-    let impulse = 10.0;
 
     for p in &mut sph.particles {
         let to_particle = p.pos - cursor_world;
-        if to_particle.length_squared() < radius2 {
-            p.vel += impulse * force_dir;
+        if to_particle.length_squared() < INTERACTION_AREA {
+            p.vel += IMPULSE * force_dir;
         }
     }
 }
 
-
-
+// all the mathematic happens here!
 fn sph_step(mut sph: ResMut<SPHState>, time: Res<Time>) {
     let dt = time.delta_secs().min(DT);
-    sph.step(dt); // integral
+    sph.step(dt, X_MAX, X_MIN, BOUNCINESS); // integral
 }
 
 fn sync_particles(
     sph: Res<SPHState>,
+    view: Res<ViewMode>,
     mut query: Query<(&ParticleVisual, &mut Transform, &mut Sprite)>,
 ) {
     let (mut min_rho, mut max_rho) = (f32::MAX, f32::MIN);
     for p in &sph.particles {
+        // find min and max density
         min_rho = min_rho.min(p.rho);
         max_rho = max_rho.max(p.rho);
     }
@@ -128,14 +156,23 @@ fn sync_particles(
 
     for (visual, mut transform, mut sprite) in query.iter_mut() {
         let particle = &sph.particles[visual.0];
+
+        // position must be matched with the Bevy world
         transform.translation.x = particle.pos.x * RENDER_SCALE;
         transform.translation.y = particle.pos.y * RENDER_SCALE;
-
-        let t = ((particle.rho - min_rho) * inv_range).clamp(0.0, 1.0);
-        sprite.color = density_color(t);
+        match *view {
+            ViewMode::ConstColor => {
+                sprite.color = CYAN;
+            }
+            ViewMode::DensityColor => {
+                let t = ((particle.rho - min_rho) * inv_range).clamp(0.0, 1.0);
+                sprite.color = density_color(t);
+            }
+        }
     }
 }
 
+// spawn a camera and particles
 fn setup(
     mut commands: Commands,
     sph: Res<SPHState>,
@@ -145,7 +182,7 @@ fn setup(
     for (i, p) in sph.particles.iter().enumerate() {
         commands.spawn((
             Sprite {
-                color: Color::srgb(0.0, 1.0, 1.0),
+                color: CYAN,
                 custom_size: Some(Vec2::splat(PARTICLE_SIZE)),
                 ..Default::default()
             },
